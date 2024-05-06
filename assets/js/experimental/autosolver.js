310ff0211a9d045285c1b536f1eec4b4c2947511
Magdamit173 <rovicmagdamit096@gmail.com>
2024-05-06 23:06:17 +0800

    version 0.0.8.2

--- assets/js/experimental/autosolver.js
+++ assets/js/experimental/autosolver.js
@@ -1,15 +1,16 @@
-function solve_script() {
+async function solve_script() {
     // const answer = `${eval(display_term.children[0].textContent.split("x").join("*"))}`
     const answer = e_answer
 
     // display_input.focus()
     display_input.value = answer
-
+    await asyncTimer(.1)
+    
     display_input.dispatchEvent(new KeyboardEvent("keyup", { key: " ", bubbles: true }))
 }
 
 function autosolve() {
-    setInterval( () => {
-        solve_script()
-    }, 100)
+    setInterval(async () => {
+        await solve_script()
+    }, 330)
 }
